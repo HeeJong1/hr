d29dd344875afea0be4e391902a855a1796aef48
HeeJong1 <84832185+HeeJong1@users.noreply.github.com>
2026-01-23 11:30:58 +0900

    css 수정

--- src/main/java/com/heejong/hr/controller/ViewController.java
+++ src/main/java/com/heejong/hr/controller/ViewController.java
@@ -71,8 +71,8 @@ public class ViewController {
         return "notice";
     }
 
-    @GetMapping("/notice/write")
+    @GetMapping("/noticeWrite")
     public String noticeWritePage() {
-        return "notice-write";
+        return "noticeWrite";
     }
 }
